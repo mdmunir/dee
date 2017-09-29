4f597ea751bf9397fb14706af41db5a2100fd552
Misbahul D Munir <misbahuldmunir@gmail.com>
2017-09-29 16:24:21 +0700

    set compatibility for phpunit < 6

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,6 +7,9 @@ $composerAutoload = __DIR__ . '/../vendor/autoload.php';
 if (is_file($composerAutoload)) {
     require_once $composerAutoload;
 }
+
 require_once __DIR__ . '/../framework/Dee.php';
 Dee::setAlias('@tests', __DIR__);
+
+require_once __DIR__ . '/compatibility.php';
 require_once __DIR__ . '/TestCase.php';
