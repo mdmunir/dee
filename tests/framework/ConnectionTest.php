e18b4042e280f7cf9be6bba9edca9473cc36becf
mdmunir <misbahuldmunir@gmail.com>
2019-10-17 16:57:44 +0700

    connection test

--- tests/framework/ConnectionTest.php
+++ tests/framework/ConnectionTest.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace tests\framework;
+
 /**
  * Description of ControllerTest
  *
@@ -8,13 +10,23 @@ namespace tests\framework;
  */
 class ConnectionTest extends \tests\TestCase
 {
+
     public function testBuildConndition()
     {
-        $db = new \dee\base\Conncetion();
-        $db->dsn = '@tests/data/dee-test.db';
-        
+        $db = new \dee\base\Connection();
+        $db->dsn = 'sqlite:@tests/data/dee-test.db';
+        $where = [
+            'kolom1' => 'value1',
+            'kolom2' => [1, 2, 3],
+        ];
+
+        $params = [];
+        $conditions = $db->buildCondition($where, $params);
+
+        $this->assertEquals('(kolom1 = :p0) AND (kolom2 IN(:p1, :p2, :p3))', $conditions);
+        $this->assertEquals([':p0' => 'value1', ':p1' => 1, ':p2' => 2, ':p3' => 3], $params);
     }
-    
+
     public function testRawSql()
     {
         
